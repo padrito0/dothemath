9fa581e580f3a51353d854ab93b6849d669f9dbf
padrito0 <146373717+padrito0@users.noreply.github.com>
2023-10-26 12:06:16 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            byte a = 25;
+            short b = 32000;
+            int c = 1000000;
+            byte x = 5;
+            byte y = 10;
+            // byte z = 2;
+            float myFloat = 1.5f;
+
+            Console.WriteLine("x + y = " + (x + y));
+            Console.WriteLine("myFloat + myFloat = " + (myFloat + myFloat));
+            Console.WriteLine("myFloat + a = " + (myFloat + a));
+            Console.WriteLine("c - b = " + (c - b));
+            Console.WriteLine("x * a = " + (x * a));
+            Console.WriteLine("y * b = " + (y * b));
+            Console.WriteLine("c / b = " + (c / b));
+            Console.WriteLine("a / x = " + (a / x));
+            Console.WriteLine("a % x = " + (a % x));
+            Console.WriteLine("c % y = " + (c % y));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+}
